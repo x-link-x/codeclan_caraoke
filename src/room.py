74a0d883971fcb3887f26a4b78d03613a576897e
Link <xlinkx_@outlook.com>
2021-05-13 14:12:42 +0100

    room has guests test passing

--- src/room.py
+++ src/room.py
@@ -2,3 +2,4 @@ class Room:
     
     def __init__(self, name):
         self.name = name
+        self.guests = {}
--- tests/room_test.py
+++ tests/room_test.py
@@ -4,8 +4,11 @@ from src.room import Room
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room_1 = Room("Room 1")
+        self.room = Room("Room 1")
 
 
     def test_room_name_is_room_1(self):
-        self.assertEqual("Room 1", self.room_1.name)
+        self.assertEqual("Room 1", self.room.name)
+
+    def test_room_has_dictionary_of_guests(self):
+        self.assertEqual({}, self.room.guests)
