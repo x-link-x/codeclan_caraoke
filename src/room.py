c292069c0243d73386c2a8a53ce8317d14decfe2
Link <xlinkx_@outlook.com>
2021-05-13 14:10:21 +0100

    room name test passing

--- src/room.py
+++ src/room.py
@@ -1,2 +1,4 @@
 class Room:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
--- tests/room_test.py
+++ tests/room_test.py
@@ -4,4 +4,8 @@ from src.room import Room
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.room_1 = Room("Room 1")
+
+
+    def test_room_name_is_room_1(self):
+        self.assertEqual("Room 1", self.room_1.name)
